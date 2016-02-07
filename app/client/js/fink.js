2189eaaae494c3e4fecc8c70be4aa104f48eb33c
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-02-07 21:41:41 +0100

    Improve backend frontend bridge

--- app/client/js/fink.js
+++ app/client/js/fink.js
@@ -1,8 +1,22 @@
 'use strict'
 
-;(function (Fink, isValid) {
+;(function (Fink, isValid, fetch) {
   Fink.foo = 'bar'
   Fink.isValidURI = function (uri) {
     return isValid(window.location.hostname, uri)
   }
-})(window.Fink, require('fink-is-valid-uri'))
+  Fink.register = function (uri) {
+    return fetch(Fink.endpoint, {
+      method: 'post',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        uri: uri
+      })
+    }).then(function (response) {
+      return response.json()
+    })
+  }
+})(window.Fink, require('fink-is-valid-uri'), window.fetch)
