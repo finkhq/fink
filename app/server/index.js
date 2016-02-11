8c3cbe78218829f55408ef73cc0b6175cc97eae0
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-02-11 14:27:44 +0100

    Use piping instead of nodemon

--- app/server/index.js
+++ app/server/index.js
@@ -3,6 +3,11 @@
 const pkg = require('../../package.json')
 const debug = require('debug')(pkg.name)
 
-require('./create')(function (url) {
-  debug(`running at ${url} 🐀`)
-})
+function run () {
+  require('./create')(function (url) {
+    debug(`running at ${url} 🐀`)
+  })
+}
+
+if (process.env.NODE_ENV === 'production') return run()
+if (require("piping")()) run()
