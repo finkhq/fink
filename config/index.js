512b533d22c2b99dcb1dc72237b4c3d91f353f1b
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-02-15 23:04:16 +0100

    Fix config values

--- config/index.js
+++ config/index.js
@@ -2,10 +2,13 @@
 
 const existsFile = require('exists-file')
 const path = require('path')
-const merge = require('lodash').merge
+const existsDefault = require('existential-default')
 
 const defaultConfig = require('./default')
-const env = process.env.NODE_ENV ? process.env.NODE_ENV.toString() : 'development'
 
-const config = existsFile(path.resolve(env)) ? merge(defaultConfig, require(env)) : defaultConfig
+const env = process.env.NODE_ENV ? process.env.NODE_ENV.toString() : 'development'
+const envFile = path.resolve('config', env + '.js')
+
+const config = existsFile(envFile) ? existsDefault(require(envFile), defaultConfig) : defaultConfig
+
 module.exports = config
