4c1f10721cb920ca34e6d2ddcf5af073ed8572ab
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-04-23 12:59:39 +0200

    Fix load default file

--- config/index.js
+++ config/index.js
@@ -1,14 +1,16 @@
 'use strict'
 
+const existsDefault = require('existential-default-deep')
 const existsFile = require('exists-file')
 const path = require('path')
-const existsDefault = require('existential-default-deep')
+const {get} = require('lodash')
 
 const defaultConfig = require('./default')
 
-const env = process.env.NODE_ENV ? process.env.NODE_ENV.toString() : 'development'
+const env = get(process.env, 'NODE_ENV', 'development')
 const envFile = path.resolve('config', env + '.js')
 
-const config = existsFile(envFile) ? existsDefault(require(envFile), defaultConfig) : defaultConfig
+const config = existsFile.sync(envFile)
+  ? existsDefault(require(envFile), defaultConfig) : defaultConfig
 
 module.exports = config
